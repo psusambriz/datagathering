875c591c4b5eabecd8861468ac04677ab2e63082
psusambriz <sambriz@pdx.edu>
2025-04-18 05:03:08 -0700

    Updated

---
+++ github.py
@@ -0,0 +1,9 @@
+import requests
+
+# REPLACE the following variables with your Client ID and Client Secret
+CLIENT_ID = "<REPLACE_WITH_CLIENT_ID>"
+CLIENT_SECRET = "<REPLACE_WITH_CLIENT_SECRET>"
+
+# REPLACE the following variable with what you added in the
+# "Authorization callback URL" field
+REDIRECT_URI = "<REPLACE_WITH_REDIRECT_URI>"
